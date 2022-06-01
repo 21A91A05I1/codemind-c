f7735a283ece4935f7ee10c75880f1b39d6b2908
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2022-06-01 11:16:34 +0530

    #include<stdio.h> int main() {     int i,k=0;     char str[100];     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>=65 && str[i]<=90)         {             k++;         }     }     printf("%d ",k); }

---
+++ uppercase_letters_in_a_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,k=0;
+    char str[100];
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>=65 && str[i]<=90)
+        {
+            k++;
+        }
+    }
+    printf("%d ",k);
+}
