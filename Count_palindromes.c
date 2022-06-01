e10e8bab20dde6e9c72908b2b88c82fa4250ec71
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2022-06-01 10:10:52 +0530

    #include<stdio.h> int palindrome(int num) {     int m,r=0,d;     m=num;     while(num!=0){         d=num%10;         num=num/10;         r=r*10+d;     }     if(r==m)     {         return 1;     }     else     {         return 0;     } } int main() {     int n,i,c=0,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(palindrome(arr[i]))         {             c++;         }     }     printf("%d",c); }

---
+++ Count_palindromes.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int palindrome(int num)
+{
+    int m,r=0,d;
+    m=num;
+    while(num!=0){
+        d=num%10;
+        num=num/10;
+        r=r*10+d;
+    }
+    if(r==m)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n,i,c=0,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(palindrome(arr[i]))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
