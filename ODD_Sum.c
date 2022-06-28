3494ecbae02d3a5ca5167017d25d819bca0147c0
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2022-06-28 21:40:58 +0530

    #include<stdio.h> int odd_sum(int *arr,int n) {     int i,sum=0;     for(i=0;i<n;i++)     {         if(arr[i]%2)         {             sum=sum+arr[i];         }     }     return sum; } int main() {     int n,i,arr[100],k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     k=odd_sum(arr,n);     printf("%d",k); }

--- ODD_Sum.c
+++ ODD_Sum.c
@@ -15,10 +15,10 @@ int main()
 {
     int n,i,arr[100],k;
     scanf("%d",&n);
-    for (i=0;i<n;i++)
+    for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
     k=odd_sum(arr,n);
-    printf("%d ",k);
+    printf("%d",k);
 }
