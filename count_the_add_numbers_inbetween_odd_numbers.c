abc79b758a58b07dcb09cdb1752f68b60a6225fc
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2022-09-10 22:33:29 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n-1;i++)     {         if(arr[i-1]%2!=0 && arr[i+1]%2!=0)         {             if(arr[i]%2!=0)             {                 c++;             }         }     }     printf("%d",c);     return 0; }

--- count_the_add_numbers_inbetween_odd_numbers.c
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
-int odd_count(int *arr,int n)
+int main()
 {
-    int i,k,c=0;
-    for(i=1;i<n-1;i++)
+    int arr[100],n,i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n-1;i++)
     {
         if(arr[i-1]%2!=0 && arr[i+1]%2!=0)
         {
@@ -12,16 +17,6 @@ int odd_count(int *arr,int n)
             }
         }
     }
-    return c;
-}
-int main()
-{
-    int n,i,arr[100],k;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    k=odd_count(arr,n);
-    printf("%d ",k);
+    printf("%d",c);
+    return 0;
 }
