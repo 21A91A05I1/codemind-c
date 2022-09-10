08ffad78b938dc8fba3c7e41e46bc89d902c3eff
21A91A05I1 <97424614+21A91A05I1@users.noreply.github.com>
2022-09-10 22:14:44 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,sum=0,odd;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if( arr[i]%2)         {             odd=arr[i];         }     }     printf("%d",odd); }

---
+++ last_odd_number_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,sum=0,odd;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if( arr[i]%2)
+        {
+            odd=arr[i];
+        }
+    }
+    printf("%d",odd);
+}
+
